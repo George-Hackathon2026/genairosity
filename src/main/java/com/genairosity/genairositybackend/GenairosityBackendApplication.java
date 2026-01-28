1e92687d539a6fef6e1edf5e0fdf29e15cb09ea4
G Tupling <bha01490631@devcorptenant.com>
2026-01-28 13:02:57 +0000

    Initial commit

---
+++ src/main/java/com/genairosity/genairositybackend/GenairosityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.genairosity.genairositybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenairosityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenairosityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genairosity/genairositybackend/GenairosityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genairosity.genairositybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenairosityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
