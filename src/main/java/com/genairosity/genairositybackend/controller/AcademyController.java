99565b3ea16005052dc3f277ed30d4d70241fd71
GTupling <BHa01490631@devcorptenant.com>
2026-01-28 17:03:27 +0000

    adds /academies endpoint

---
+++ src/main/java/com/genairosity/genairositybackend/controller/AcademyController.java
@@ -0,0 +1,29 @@
+package com.genairosity.genairositybackend.controller;
+
+import com.genairosity.genairositybackend.entity.Academy;
+import com.genairosity.genairositybackend.entity.City;
+import com.genairosity.genairositybackend.repository.AcademyRepository;
+import com.genairosity.genairositybackend.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AcademyController {
+
+    @Autowired
+    private AcademyRepository academyRepository;
+
+    @GetMapping("/academies")
+    public List<Academy> getCities() {
+        List<Academy> academies = new ArrayList<>();
+        Iterable<Academy> iterable = academyRepository.findAll();
+        iterable.forEach(academies::add);
+        return academies;
+    }
+}
