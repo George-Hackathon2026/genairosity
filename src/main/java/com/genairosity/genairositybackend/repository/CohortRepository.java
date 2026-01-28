a7762746140d7f689e7fb4d7aa7bf131717a5a3d
GTupling <BHa01490631@devcorptenant.com>
2026-01-28 20:58:17 +0000

    bug fix

--- src/main/java/com/genairosity/genairositybackend/repository/CohortRepository.java
+++ src/main/java/com/genairosity/genairositybackend/repository/CohortRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface CohortRepository extends CrudRepository<Cohort, Integer> {
-    @Query("SELECT * FROM hackathon_teams.genairosity.cohort WHERE is_active = true AND status != 'Complete'")
+    @Query("SELECT * FROM hackathon_teams.genairosity.cohort WHERE is_active = true AND status != 'Completed'")
     List<Cohort> findAllActive();
 }
