e9c33e8e421f4a81d1de30de013f5bb4b0263532
GTupling <BHa01490631@devcorptenant.com>
2026-01-28 20:07:57 +0000

    adds GET /api/cohort/{cohortId} method

--- src/main/java/com/genairosity/genairositybackend/controller/CohortController.java
+++ src/main/java/com/genairosity/genairositybackend/controller/CohortController.java
@@ -3,12 +3,15 @@ package com.genairosity.genairositybackend.controller;
 import com.genairosity.genairositybackend.entity.Cohort;
 import com.genairosity.genairositybackend.repository.CohortRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +26,13 @@ public class CohortController {
         Iterable<Cohort> iterable = cohortRepository.findAllActive();
         iterable.forEach(cohorts::add);
         return cohorts;
+    }
+
+    @GetMapping("/cohort/{cohortId}")
+    public ResponseEntity<Cohort> getCohort(@PathVariable("cohortId") Integer cohortId) {
+        Optional<Cohort> optional = cohortRepository.findById(cohortId);
+
+        return optional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
